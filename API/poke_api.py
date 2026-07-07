0344c1205109fd122298ec9f6110a774a7e76d72
JayDaslani <jaydashlani@gmail.com>
2026-07-07 13:45:12 +0530

    GET request with Poke_Api

---
+++ API/poke_api.py
@@ -0,0 +1,17 @@
+import requests
+
+url = 'https://pokeapi.co/api/v2/pokemon/pikachu'
+
+response = requests.get(url)
+
+print(f"Status: {response.status_code}")
+data = response.json()
+print(f"Name: {data['name']}")
+print(f"Height: {data['height']}")
+print(f"Weight: {data['weight']}")
+print(f"Type: {data['types'][0]['type']['name']}")
+
+# Stats nikalo
+print("\nStats:")
+for stat in data['stats']:
+    print(f"  {stat['stat']['name']}: {stat['base_stat']}")
