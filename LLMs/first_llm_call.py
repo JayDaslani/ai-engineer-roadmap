3947c1dbeddf95df20547e6229b2bffa4a631032
JayDaslani <jaydashlani@gmail.com>
2026-07-16 10:54:19 +0530

    first LLm call and system prompt

---
+++ LLMs/first_llm_call.py
@@ -0,0 +1,28 @@
+
+
+
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpfool assistant give answer only in english"
+        },
+        {
+            "role": "user",
+            "content": "What are your name?"
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
